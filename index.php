089344e3e2d068b734ecfe36527f7236972c605a
HannesSjo <hannes.sjo2@gmail.com>
2018-02-02 13:50:05 +0200

    Began working on splash screen

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 $bodyClass = "d-flex justify-content-center align-items-center";
 $Whateva = "Bästa att göra listan någonsin";
-include 'includes/header.php';?>
+include 'includes/header.php';
+
+$query = "SELECT * FROM users";
+$result = mysqli_query($connection, $query);
+$num_users = mysqli_num_rows($result);
+?>
 
 
 <!-- Background video -->
-<video autoplay loop muted >
+<video autoplay loop muted>
   <source src="vid/Video.mp4" type="video/mp4">
   Your browser does not support the video tag.
 </video>
@@ -14,6 +19,9 @@ include 'includes/header.php';?>
 <!-- Main content -->
 <main class="animated flipInY">
   <img src="img/todoLogo.svg" alt="">
+  <a class="btn btn-primary" href="login.php">Login</a>
+  <a class="btn btn-secondary" href="register.php">Register</a>
+  <p>Med <?php echo $num_users ?> registrerade användare</p>
 </main>
 <!-- Main content END -->
 
